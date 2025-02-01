a6110c952358baee0232c59fd59b4b716f1f0c0e
Devansh Khandelwal <devanshksg@gmail.com>
2025-02-02 01:14:30 +0530

    feat: setup firebase config

--- src/students/interfaces/IAuthRepository.ts
+++ src/students/interfaces/IAuthRepository.ts
@@ -2,8 +2,7 @@ import { IStudent } from "./IStudent";
 
 export interface IAuthRepository {
     findByEmail(email: string): Promise<IStudent | null>;
-    create(student: IStud
-        ent): Promise<IStudent>;
+    create(student: IStudent): Promise<IStudent>;
     updatePassword(id: string, password: string): Promise<void>;
     storeOTP(email: string, otp: string): Promise<void>;
     verifyOTP(email: string, otp: string): Promise<boolean>;
