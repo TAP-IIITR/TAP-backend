06817f19d012299132ee46b7b41e2f80e24f55b1
Devansh Khandelwal <devanshksg@gmail.com>
2025-03-22 17:28:11 +0530

    feat : updated cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow your frontend origin
+    origin: "*", // Allow your frontend origin
     credentials: true, // Allow cookies/credentials to be sent
   })
 );
