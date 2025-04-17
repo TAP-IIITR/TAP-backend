fe02c0391fe27883d2d783f469a0c6b736ed8031
Shubh-2909 <shubhshubhanjal96@gmail.com>
2025-04-17 11:46:10 +0530

    get -> post in logout route

--- src/tap-coords/routes/auth.routes.ts
+++ src/tap-coords/routes/auth.routes.ts
@@ -1,37 +1,34 @@
-import { Router } from 'express';
-import { body } from 'express-validator';
-import { validateRequest } from '../../middleware/validation.middleware';
-import { checkAuth } from '../../middleware/auth.middleware';
-import { register, login, logout, resetPassword } from '../controllers/auth.controllers';
+import { Router } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../../middleware/validation.middleware";
+import { checkAuth } from "../../middleware/auth.middleware";
+import {
+  register,
+  login,
+  logout,
+  resetPassword,
+} from "../controllers/auth.controllers";
 
 const router = Router();
 
 router.post(
-  '/register',
+  "/register",
   [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('email').isEmail().withMessage('Please provide a valid email'),
-    body('password').notEmpty().withMessage('Password is required'),
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Please provide a valid email"),
+    body("password").notEmpty().withMessage("Password is required"),
   ],
   validateRequest,
   register
 );
 
-router.post(
-  '/login',
-  [
-  ],
-  validateRequest,
-  login
-);
+router.post("/login", [], validateRequest, login);
 
-router.get('/logout', checkAuth, logout);
+router.post("/logout", checkAuth, logout);
 
 router.post(
-  '/reset-password',
-  [
-    body('email').isEmail().withMessage('Please provide a valid email'),
-  ],
+  "/reset-password",
+  [body("email").isEmail().withMessage("Please provide a valid email")],
   validateRequest,
   resetPassword
 );
