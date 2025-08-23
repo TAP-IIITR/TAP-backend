60944cc3e55348100da194f6292fd52de8be4d01
TAP-IIITR <tpo@iiitranchi.ac.in>
2025-08-23 16:01:48 +0530

    typecast number

--- src/index.ts
+++ src/index.ts
@@ -73,7 +73,7 @@ app.get("/", (req, res) => {
 // Error handler middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
